0ba403e5ae309b8afa9781ba6f52eab03d8ab418
Aravindakumar2002 <aravindakumar.mahesh2002@gmail.com>
2023-04-25 19:08:09 +0530

    javascripting finish

---
+++ functions.js
@@ -0,0 +1,4 @@
+function eat(food){
+    return console.log(food + " tasted really good.")
+}
+eat("bananas")
--- variables.js
+++ variables.js
@@ -1,2 +1,50 @@
 a="some string"
 console.log(a)
+
+
+
+/* .con{
+    background-color: blue;
+    display: flex; */
+
+/* .head1{
+    /* background-image: url(./images/Tula\ Pink-\ Solids-\ cornflower-\ CSFSESS-CT123015-\ Modern\ Cotton\ Yardage.jpeg); */
+    background-color: blue;
+    /* position: fixed;
+    background: cover;
+    background-repeat: no-repeat; 
+    width: 100%;
+    height: 75%;
+    padding: 50px;
+}
+.headmsg1{
+    color: white;
+    padding-top: 80px;
+    padding-left: 80px;
+    font-weight: bold;
+}
+.headmsg2{
+    color: white;
+    padding-left: 80px;
+    /* margin:80p */
+    font-weight: bold;
+}
+/* .header img{
+    display: flex;
+    justify-content: flex-end;
+    border-radius: 100px;
+    width: 40%;
+    height: 70%;
+} 
+.headimg{
+    /* display: flex;
+    justify-content: flex-end;
+    justify-items: center;
+    flex-direction: row-reverse;
+    border-radius: 50%;
+    width: 20%;
+    height: 60%;
+}
+con{
+    display: flex;
+} 
